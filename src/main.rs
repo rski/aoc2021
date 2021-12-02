ab19ff0fbe526c9881167dbfecb2c8934a5c1f05
Romanos Skiadas <rom.skiad@gmail.com>
2021-12-02 21:45:23 +0200

    add day2

--- src/main.rs
+++ src/main.rs
@@ -28,8 +28,59 @@ fn aoc1(w_size: usize) -> std::io::Result<()> {
     Ok(())
 }
 
+struct Subnautica {
+    d: i32,
+    fwd: i32,
+    aim: i32,
+    aim_depth: i32,
+}
+
+fn aoc2() -> std::io::Result<()> {
+    let mut f = File::open("d2.in")?;
+    let mut buffer = String::new();
+    f.read_to_string(&mut buffer)?;
+    let directions = buffer.lines().map(|l| {
+        let mut it = l.split_ascii_whitespace();
+        let direction = it.next().unwrap();
+        let v = it.next().unwrap().parse::<i32>().unwrap();
+        (direction, v)
+    });
+
+    let mut s = Subnautica {
+        d: 0,
+        fwd: 0,
+        aim: 0,
+        aim_depth: 0,
+    };
+    for d in directions {
+        match d {
+            ("forward", x) => {
+                s.fwd += x;
+                s.aim_depth += s.aim * x;
+            }
+            ("down", x) => {
+                s.d += x;
+                s.aim += x;
+            }
+            ("up", x) => {
+                s.d -= x;
+                s.aim -= x;
+            }
+            (d, _) => panic!("unexpected direction {}", d),
+        };
+    }
+    println!(
+        "aoc2_p1: {}, aim_depth:{}",
+        s.d * s.fwd,
+        s.fwd * s.aim_depth
+    );
+
+    Ok(())
+}
+
 fn main() -> std::io::Result<()> {
     aoc1(1)?;
     aoc1(3)?;
+    aoc2()?;
     Ok(())
 }
