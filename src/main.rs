c30dcba66090ef15a91b6f98f0fcaf98e41fd037
Romanos Skiadas <rom.skiad@gmail.com>
2021-12-04 06:32:27 +0200

    add day 3

--- src/main.rs
+++ src/main.rs
@@ -78,9 +78,113 @@ fn aoc2() -> std::io::Result<()> {
     Ok(())
 }
 
+fn get_numbers(b: &String) -> (Vec<u64>, u64) {
+    let mut numbers: Vec<u64> = vec![];
+    let mut lines = 0;
+    let mut width = None;
+    for l in b.lines() {
+        let mut bp = Vec::<u8>::new();
+        lines += 1;
+        for c in l.chars() {
+            match c {
+                '0' => {
+                    bp.push(0_u8);
+                }
+                '1' => {
+                    bp.push(1_u8);
+                }
+                '\n' => {}
+                _ => panic!("unexpected"),
+            };
+        }
+        let mut a = 0_u64;
+        for (i, v) in bp.iter().rev().enumerate() {
+            a |= (*v as u64) << i;
+        }
+        width = Some(bp.len());
+        numbers.push(a);
+    }
+    (numbers, width.unwrap() as u64)
+}
+
+fn db(n: &Vec<u64>) {
+    for i in n.iter() {
+        print!("{:08b}/{} ", i, i);
+    }
+    println!()
+}
+
+fn recursive_filter(numbers: &Vec<u64>, width: u64, flip: bool) -> u64 {
+    let mut n = numbers.to_vec();
+    for i in 0..width {
+        let pos = width - i - 1;
+        let pop_bit_pattern = most_popular_bits_pattern(&n, width); // this is calculated a bunch of types for initial n, but eh
+        let mut v = pop_bit_pattern[i as usize];
+        if flip {
+            v = !v;
+        }
+        let s: u64 = v.into();
+        dbg!(s);
+        db(&n);
+        n = n
+            .iter()
+            .filter(|&x| ((*x >> pos) & 1 == s))
+            .map(|&x| x)
+            .collect();
+        db(&n);
+        if n.len() <= 1 {
+            break;
+        }
+    }
+
+    if n.len() != 1 {
+        panic!("wanted 1 number left, {:?} ", n)
+    }
+
+    n.pop().unwrap()
+}
+
+fn most_popular_bits_pattern(nums: &Vec<u64>, width: u64) -> Vec<bool> {
+    let mut vals: Vec<u64> = vec![];
+
+    for i in 0..width {
+        vals.push(0);
+        for n in nums {
+            vals[i as usize] += (n >> i) & 1
+        }
+    }
+    let s = nums.len();
+    vals.iter().rev().map(|x| x * 2 >= s as u64).collect()
+}
+
+fn aoc3() -> std::io::Result<()> {
+    let mut f = File::open("d3.in")?;
+    let mut buffer = String::new();
+    f.read_to_string(&mut buffer)?;
+    let (numbers, width) = get_numbers(&buffer);
+    let vals = most_popular_bits_pattern(&numbers, width);
+
+    let mut gamma = 0_u64;
+    let mut epsilon = 0_u64;
+    for (i, v) in vals.iter().rev().enumerate() {
+        let v: u64 = (*v).into();
+        gamma |= v << i;
+        epsilon |= (v ^ 1) << i;
+    }
+
+    println!("{}, {}, {}", gamma, epsilon, gamma * epsilon);
+    println!("{:?}", numbers);
+
+    let oxy = recursive_filter(&numbers, width, false);
+    let co2 = recursive_filter(&numbers, width, true);
+    dbg!(oxy, co2, oxy * co2);
+    Ok(())
+}
+
 fn main() -> std::io::Result<()> {
     aoc1(1)?;
     aoc1(3)?;
     aoc2()?;
+    aoc3()?;
     Ok(())
 }
