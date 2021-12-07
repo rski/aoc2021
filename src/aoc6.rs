b1948c04332948a7557e214f1b0b6a8e385ff04b
Romanos Skiadas <rom.skiad@gmail.com>
2021-12-07 16:53:10 +0200

    aoc6: completely remove Fish

--- src/aoc6.rs
+++ src/aoc6.rs
@@ -1,8 +1,6 @@
 use std::{collections::HashMap, fs::read_to_string};
 
-type Fish = i64;
-
-fn produce(first_day: i64) -> Vec<Fish> {
+fn produce(first_day: i64) -> Vec<i64> {
     let mut i = 1;
     let mut fishes = vec![];
     loop {
@@ -29,11 +27,11 @@ fn reproduce(first_day: i64, mut cache: &mut HashMap<i64, usize>) -> usize {
 
 fn main() -> std::io::Result<()> {
     let buffer = read_to_string(String::from("d6.in"))?;
-    let state: Vec<Fish> = buffer
+    let state: Vec<i64> = buffer
         .strip_suffix("\n")
         .unwrap()
         .split(",")
-        .map(|x| x.parse::<Fish>().unwrap() - 6)
+        .map(|x| x.parse::<i64>().unwrap() - 6)
         .collect();
     println!("Initial state: {:?}", state);
 
