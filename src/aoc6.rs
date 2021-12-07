37323bb74f31fe964a03437ff8b345d0dc85ee12
Romanos Skiadas <rom.skiad@gmail.com>
2021-12-07 16:07:52 +0200

    remove fish, add cache stats

--- src/aoc6.rs
+++ src/aoc6.rs
@@ -1,30 +1,19 @@
 use std::{collections::HashMap, fs::read_to_string};
 
-#[derive(Clone)]
-// The i64 is the day the fish's counteDay ready is the first day the fish's counter was 6. This is negative for initial state fish.
-struct Fish(i64);
+type Fish = i64;
 
-impl std::fmt::Debug for Fish {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        write!(f, "{}", self.0)?;
-        Ok(())
-    }
-}
-
-impl Fish {
-    fn reproduce(&self) -> Vec<Fish> {
-        let mut i = 1;
-        let mut fishes = vec![];
-        loop {
-            let dob = i * 7 + self.0;
-            if dob > 80 {
-                break;
-            }
-            i += 1;
-            fishes.push(Fish(dob + 2))
+fn reproduce(first_day: i64) -> Vec<i64> {
+    let mut i = 1;
+    let mut fishes = vec![];
+    loop {
+        let dob = i * 7 + first_day;
+        if dob > 80 {
+            break;
         }
-        fishes
+        i += 1;
+        fishes.push(dob + 2)
     }
+    fishes
 }
 
 fn main() -> std::io::Result<()> {
@@ -33,32 +22,40 @@ fn main() -> std::io::Result<()> {
         .strip_suffix("\n")
         .unwrap()
         .split(",")
-        .map(|x| Fish(x.parse::<i64>().unwrap() - 6))
+        .map(|x| x.parse::<Fish>().unwrap() - 6)
         .collect();
     println!("Initial state: {:?}", state);
 
     let mut num_fishes = state.len();
     let mut cache = HashMap::<i64, Vec<Fish>>::new();
+    let (mut cache_hits, mut cache_inserts) = (0, 0);
     loop {
         let mut new_fish: Vec<Fish> = vec![];
         if state.len() == 0 {
             break;
         }
         for f in state {
-            let cached = cache.get(&f.0);
+            let cached = cache.get(&f);
             let mut this_fish = match cached {
                 None => {
-                    let new = f.reproduce();
-                    cache.insert(f.0, new.to_owned());
+                    let new = reproduce(f);
+                    cache_inserts += 1;
+                    cache.insert(f, new.to_owned());
                     new
                 }
-                Some(x) => x.clone(),
+                Some(x) => {
+                    cache_hits += 1;
+                    x.clone()
+                }
             };
             num_fishes += this_fish.len();
             new_fish.append(&mut this_fish);
         }
         state = new_fish;
     }
-    println!("{} fish now", num_fishes);
+    println!(
+        "{} fish now ({} hits, {} inserts)",
+        num_fishes, cache_hits, cache_inserts
+    );
     Ok(())
 }
