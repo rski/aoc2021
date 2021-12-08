8cdd0bb5329f69776dad0a0125563070437a8400
Romanos Skiadas <rom.skiad@gmail.com>
2021-12-08 16:20:23 +0200

    aoc8: do part2

--- src/aoc8.rs
+++ src/aoc8.rs
@@ -1,25 +1,89 @@
-use std::{collections::HashMap, fs::read_to_string};
+use std::{
+    collections::{BTreeSet, HashMap},
+    fs::read_to_string,
+    ops::Sub,
+};
 
 fn aoc(input: &str) {
-    let width_to_number = HashMap::from([(2, 1), (3, 7), (4, 4), (7, 8)]);
-    let mut freq = HashMap::<u64, u64>::new();
+    let width_to_number = HashMap::from([(2, 1), (4, 4), (3, 7), (7, 8)]);
+    let mut vals = 0;
     for l in input.lines() {
-        let split = l.split("|");
-        let output = split.skip(1).next().unwrap();
-        for pattern in output.split(" ") {
-            if pattern == "" {
-                continue;
-            }
-            match width_to_number.get(&pattern.chars().count()) {
+        let mut sets = HashMap::<u64, BTreeSet<char>>::new();
+        let mut split = l.split("|");
+        let all_patterns = split.next().unwrap().trim().split(" ");
+        for pattern in all_patterns.clone() {
+            let w = &pattern.chars().count();
+            match width_to_number.get(w) {
                 None => {}
                 Some(x) => {
-                    let prev = freq.get(&x).unwrap_or(&0) + 1;
-                    freq.insert(*x, prev);
+                    let mut s = BTreeSet::new();
+                    for c in pattern.chars() {
+                        s.insert(c);
+                    }
+                    sets.insert(*x, s);
                 }
             }
         }
+        for pattern in all_patterns.clone() {
+            if pattern.chars().count() != 6 {
+                continue;
+            }
+            let four = sets.get(&4).unwrap();
+            let mut s = BTreeSet::new();
+            for c in pattern.chars() {
+                s.insert(c);
+            }
+            if four.is_subset(&s) {
+                sets.insert(9, s);
+            } else {
+                let one = sets.get(&1).unwrap();
+                if one.is_subset(&s) {
+                    sets.insert(0, s);
+                } else {
+                    sets.insert(6, s);
+                }
+            }
+        }
+        for pattern in all_patterns.clone() {
+            if pattern.chars().count() != 5 {
+                continue;
+            }
+
+            let mut s = BTreeSet::new();
+            for c in pattern.chars() {
+                s.insert(c);
+            }
+            let one = sets.get(&1).unwrap();
+            if one.is_subset(&s) {
+                sets.insert(3, s);
+            } else {
+                let six = sets.get(&6).unwrap();
+                if s.sub(&six).len() == 0 {
+                    sets.insert(5, s);
+                } else {
+                    sets.insert(2, s);
+                }
+            }
+        }
+        let mut rev_sets = HashMap::<BTreeSet<char>, u64>::new();
+        for (k, v) in sets {
+            rev_sets.insert(v, k);
+        }
+        let output = split.next().unwrap().trim().split(" ");
+        let mut day_value: u64 = 0;
+        for (i, pattern) in output.enumerate() {
+            let mut s = BTreeSet::new();
+            for c in pattern.chars() {
+                s.insert(c);
+            }
+            let v = rev_sets.get(&s).unwrap();
+            day_value += 10_u64.pow(4 - 1 - i as u32) * v;
+            dbg!(pattern, v);
+        }
+        vals += day_value
     }
-    println!("total:{}", freq.values().sum::<u64>());
+
+    println!("total:{:?}", vals);
 }
 fn main() -> std::io::Result<()> {
     let test = "\
