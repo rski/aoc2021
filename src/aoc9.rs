0b615a18f295a100a16d1126bfbf77661a9032d5
Romanos Skiadas <rom.skiad@gmail.com>
2021-12-09 15:27:53 +0200

    aoc9: do some cleanup

--- src/aoc9.rs
+++ src/aoc9.rs
@@ -1,6 +1,6 @@
 use std::{collections::BTreeSet, fs::read_to_string};
 
-fn aoc(s: &str) {
+fn aoc(s: &str) -> (usize, usize) {
     let mut nums: Vec<Vec<u32>> = vec![];
     for l in s.lines() {
         dbg!(&l);
@@ -11,10 +11,9 @@ fn aoc(s: &str) {
     let mut basins: Vec<(usize, usize)> = vec![];
     for (row, vec) in nums.iter().enumerate() {
         for (i, n) in vec.iter().enumerate() {
-            let is_min = neighbors(row, i, l_size, &nums).iter().all(|&x| match x {
-                None => true,
-                Some((row, i)) => *n < nums[row][i],
-            });
+            let is_min = neighbors(row, i, l_size, &nums)
+                .iter()
+                .all(|&x| *n < nums[x.0][x.1]);
             if !is_min {
                 continue;
             }
@@ -23,21 +22,19 @@ fn aoc(s: &str) {
         }
     }
     let risk: u32 = basins.iter().map(|(row, i)| nums[*row][*i]).sum::<u32>() + basins.len() as u32;
-    let mut basin_sizes: Vec<usize> = basins
+    let mut basin_sizes = basins
         .iter()
-        .map(|s| calc_basin(*s, l_size, &nums).len())
+        .map(|&s| calc_basin(s, l_size, &nums))
         .collect::<Vec<usize>>();
 
     basin_sizes.sort();
-    dbg!(basin_sizes.iter().rev().take(3).product::<usize>());
-    dbg!(risk);
+    (
+        risk as usize,
+        basin_sizes.iter().rev().take(3).product::<usize>(),
+    )
 }
 
-fn calc_basin(
-    seed: (usize, usize),
-    l_size: usize,
-    points: &Vec<Vec<u32>>,
-) -> BTreeSet<(usize, usize)> {
+fn calc_basin(seed: (usize, usize), l_size: usize, points: &Vec<Vec<u32>>) -> usize {
     let mut basin = BTreeSet::<(usize, usize)>::new();
     basin.insert(seed);
     let mut prev_set = basin.clone();
@@ -45,64 +42,36 @@ fn calc_basin(
         if prev_set.len() == 0 {
             break;
         }
-        for e in &prev_set {
-            basin.insert(*e);
-        }
         let mut new_points = BTreeSet::<(usize, usize)>::new();
         for p in &prev_set {
-            let neighbors = neighbors(p.0, p.1, l_size, points);
-            for n in &neighbors {
-                match n {
-                    None => {}
-                    Some(n) => {
-                        if points[n.0][n.1] != 9 && !basin.contains(n) {
-                            new_points.insert(*n);
-                        };
-                    }
+            for n in &neighbors(p.0, p.1, l_size, points) {
+                if points[n.0][n.1] != 9 && !basin.contains(n) {
+                    basin.insert(*n);
+                    new_points.insert(*n);
                 }
             }
         }
         prev_set = new_points;
     }
-    basin
+    basin.len()
 }
 
-fn neighbors(
-    row: usize,
-    i: usize,
-    l_len: usize,
-    points: &Vec<Vec<u32>>,
-) -> [Option<(usize, usize)>; 4] {
-    [
-        {
-            if row == 0 {
-                None
-            } else {
-                Some((row - 1, i))
-            }
-        },
-        {
-            if row + 1 == points.len() {
-                None
-            } else {
-                Some((row + 1, i))
-            }
-        },
-        {
-            if i == 0 {
-                None
-            } else {
-                Some((row, i - 1))
-            }
-        },
-        {
-            if i + 1 == l_len {
-                None
-            } else {
-                Some((row, i + 1))
-            }
-        },
-    ]
+fn neighbors(row: usize, i: usize, l_len: usize, points: &Vec<Vec<u32>>) -> Vec<(usize, usize)> {
+    let mut n = Vec::<(usize, usize)>::new();
+    if row != 0 {
+        n.push((row - 1, i));
+    }
+
+    if row + 1 != points.len() {
+        n.push((row + 1, i));
+    }
+    if i != 0 {
+        n.push((row, i - 1))
+    }
+    if i + 1 != l_len {
+        n.push((row, i + 1));
+    }
+    n
 }
 
 fn main() -> std::io::Result<()> {
@@ -113,8 +82,12 @@ fn main() -> std::io::Result<()> {
 8767896789
 9899965678
 ";
-    aoc(test);
+    let (risk, basins) = aoc(test);
+    assert_eq!(risk, 15);
+    assert_eq!(basins, 1134);
     let buffer = read_to_string(String::from("d9.in"))?;
-    aoc(buffer.trim());
+    let (risk, basins) = aoc(buffer.trim());
+    assert_eq!(risk, 506);
+    assert_eq!(basins, 931200);
     Ok(())
 }
