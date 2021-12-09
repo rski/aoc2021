8c83ec039bb46db1a8f846b0ce1373eecd004823
Romanos Skiadas <rom.skiad@gmail.com>
2021-12-09 12:41:51 +0200

    aoc9: refactor to make pt2 easier

--- src/aoc9.rs
+++ src/aoc9.rs
@@ -8,21 +8,60 @@ fn aoc(s: &str) {
         nums.push(v);
     }
     let l_size = nums[0].len();
-    let mut mins: Vec<u32> = vec![];
+    let mut basins: Vec<(usize, usize)> = vec![];
     for (row, vec) in nums.iter().enumerate() {
         for (i, n) in vec.iter().enumerate() {
-            if (row == 0 || *n < nums[row - 1][i])
-                && (row + 1 >= nums.len() || *n < nums[row + 1][i])
-                && (i == 0 || *n < vec[i - 1])
-                && (i + 1 >= l_size || *n < vec[i + 1])
-            {
-                mins.push(*n);
-                println!("line {} i {} number {} is min", i / l_size, i % l_size, n);
+            let is_min = neighbors(row, i, l_size, &nums).iter().all(|&x| match x {
+                None => true,
+                Some((row, i)) => *n < nums[row][i],
+            });
+            if !is_min {
+                continue;
             }
+            basins.push((row, i));
+            println!("line {} i {} number {} is min", i / l_size, i % l_size, n);
         }
     }
-    let risk: u32 = mins.iter().sum::<u32>() + mins.len() as u32;
-    dbg!(&mins, risk);
+    let risk: u32 = basins.iter().map(|(row, i)| nums[*row][*i]).sum::<u32>() + basins.len() as u32;
+    dbg!(&basins, risk);
+}
+
+fn neighbors(
+    row: usize,
+    i: usize,
+    l_len: usize,
+    points: &Vec<Vec<u32>>,
+) -> [Option<(usize, usize)>; 4] {
+    [
+        {
+            if row == 0 {
+                None
+            } else {
+                Some((row - 1, i))
+            }
+        },
+        {
+            if row + 1 == points.len() {
+                None
+            } else {
+                Some((row + 1, i))
+            }
+        },
+        {
+            if i == 0 {
+                None
+            } else {
+                Some((row, i - 1))
+            }
+        },
+        {
+            if i + 1 == l_len {
+                None
+            } else {
+                Some((row, i + 1))
+            }
+        },
+    ]
 }
 
 fn main() -> std::io::Result<()> {
